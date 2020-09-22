ebbf40e35a4fa1a3a7378ac7dca1c45552366efe
username <17801190934@163.com>
2020-09-22 12:49:36 +0800

    5.swagger2配置②

--- guli_parent/service/service_edu/src/main/java/com/atguigu/guli/service/edu/controller/admin/TeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/guli/service/edu/controller/admin/TeacherController.java
@@ -3,7 +3,12 @@ package com.atguigu.guli.service.edu.controller.admin;
 
 import com.atguigu.guli.service.edu.entity.Teacher;
 import com.atguigu.guli.service.edu.service.TeacherService;
+import com.sun.org.apache.regexp.internal.RE;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +21,8 @@ import java.util.List;
  * @author Helen
  * @since 2020-09-19
  */
+
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/admin/edu/teacher")
 public class TeacherController {
@@ -23,13 +30,15 @@ public class TeacherController {
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation("所有讲师列表")
     @GetMapping("list")
     public List<Teacher> listAll() {
         return teacherService.list();
     }
 
+    @ApiOperation(value = "根据ID删除讲师",notes = "根据ID删除讲师，逻辑删除")
     @DeleteMapping("remove/{id}")
-    public Boolean removeById(@PathVariable String id) {
+    public boolean removeById(@ApiParam(value = "讲师ID",required = true) @PathVariable String id) {
         return teacherService.removeById(id);
     }
 
